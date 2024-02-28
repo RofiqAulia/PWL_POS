69743b6e46bfada8baca7585a15244de6541a5b1
= <=>
2024-02-28 10:31:16 +0700

    first commit

---
+++ database/migrations/2024_02_28_032559_create_m_kategori_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_kategori', function (Blueprint $table) {
+            $table->id('id_kategori');
+            $table->string('kategori_kode', 10);
+            $table->string('kategori_nama', 100);
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_kategori');
+    }
+};
