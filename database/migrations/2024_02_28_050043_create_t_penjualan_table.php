6454815a384797ca2ddf0713714bb2be9a5c6c92
= <=>
2024-02-28 12:02:36 +0700

    first commit

--- database/migrations/2024_02_28_050043_create_t_penjualan_table.php
+++ database/migrations/2024_02_28_050043_create_t_penjualan_table.php
@@ -12,10 +12,16 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('t_penjualan', function (Blueprint $table) {
-            $table->id();
+            $table->id('penjualan_id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('user_id')->on('m_user');
+            $table->string('pembeli', 50);
+            $table->string('penjualan_kode', 20);
+            $table->dateTime('penjualan_tanggal');
             $table->timestamps();
         });
     }
+    
 
     /**
      * Reverse the migrations.
