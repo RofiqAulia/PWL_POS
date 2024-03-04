046427a3d60e023175c195c26a5a2dc4cc9fe3ea
= <=>
2024-03-04 17:24:50 +0700

    p

--- database/migrations/2024_02_28_050051_create_t_penjualan_detail_table.php
+++ database/migrations/2024_02_28_050051_create_t_penjualan_detail_table.php
@@ -13,13 +13,14 @@ return new class extends Migration
     {
         Schema::create('t_penjualan_detail', function (Blueprint $table) {
             $table->bigIncrements('detail_id');
-            $table->unsignedBigInteger('penjualan_id');
-            $table->foreign('penjualan_id')->references('penjualan_id')->on('t_penjualan');
+            $table->unsignedBigInteger('barang_id')->index();
             $table->unsignedBigInteger('barang_id');
-            $table->foreign('barang_id')->references('barang_id')->on('m_barang');
             $table->integer('harga');
             $table->integer('jumlah');
             $table->timestamps();
+
+            $table->foreign('penjualan_id')->references('penjualan_id')->on('t_penjualan');
+            $table->foreign('barang_id')->references('barang_id')->on('m_barang');
         });
     }
 
