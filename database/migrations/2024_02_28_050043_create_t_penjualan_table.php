0af4edddc7edc3cd9b20b8baa33240911e13b986
= <=>
2024-03-04 17:30:01 +0700

    second commit

--- database/migrations/2024_02_28_050043_create_t_penjualan_table.php
+++ database/migrations/2024_02_28_050043_create_t_penjualan_table.php
@@ -11,16 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('t_penjualan_detail', function (Blueprint $table) {
-            $table->id('detail_id');
-            $table->unsignedBigInteger('penjualan_id')->index();
-            $table->unsignedBigInteger('barang_id')->index();
-            $table->integer('harga');
-            $table->integer('jumlah');
+        Schema::create('t_penjualan', function (Blueprint $table) {
+            $table->id('penjualan_id');
+            $table->unsignedBigInteger('user_id')->index();
+            $table->string('pembeli', 50);
+            $table->string('penjualan_kode', 20);
+            $table->dateTime('penjualan_tanggal');
             $table->timestamps();
 
-            $table->foreign('penjualan_id')->references('penjualan_id')->on('t_penjualan');
-            $table->foreign('barang_id')->references('barang_id')->on('m_barang');
+            $table->foreign('user_id')->references('user_id')->on('m_user');
         });
     }
     
