d19ed8966612f295c0f8d574607a3803517021de
= <=>
2024-03-04 23:43:08 +0700

    ubah

--- database/migrations/2024_02_28_050020_create_m_barang_table.php
+++ database/migrations/2024_02_28_050020_create_m_barang_table.php
@@ -13,14 +13,7 @@ return new class extends Migration
     {
         Schema::create('m_barang', function (Blueprint $table) {
             $table->id('barang_id');
-            $table->unsignedBigInteger('kategori_id')->index();
-            $table->string('barang_kode', 10);
-            $table->string('barang_nama', 100);
-            $table->integer('harga_beli');
-            $table->integer('harga_jual');
             $table->timestamps();
-
-            $table->foreign('kategori_id')->references('kategori_id')->on('m_kategori');
         });
     }
 
--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -16,47 +16,27 @@ class BarangSeeder extends Seeder
         // Data seed untuk tabel m_barang
         $barangData = [
             [
-                'kategori_id' => 1, // Sesuaikan dengan ID kategori Digital
-                'barang_kode' => 'DGT',
-                'barang_nama' => 'HandPhone',
-                'harga_beli' => 5000000,
-                'harga_jual' => 8000000,
+                'barang_id' => 1, // Sesuaikan dengan ID kategori Digital
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'kategori_id' => 1, // Sesuaikan dengan ID kategori Elektronik
-                'barang_kode' => 'ELNK',
-                'barang_nama' => 'Kompor Listrik',
-                'harga_beli' => 3000000,
-                'harga_jual' => 4500000,
+                'barang_id' => 1, // Sesuaikan dengan ID kategori Elektronik
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'kategori_id' => 2, // Sesuaikan dengan ID kategori Plastik
-                'barang_kode' => 'PLST',
-                'barang_nama' => 'Tupper Ware',
-                'harga_beli' => 200000,
-                'harga_jual' => 500000,
+                'barang_id' => 2, // Sesuaikan dengan ID kategori Plastik
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'kategori_id' => 2, // Sesuaikan dengan ID kategori Fashion
-                'barang_kode' => 'FSH',
-                'barang_nama' => 'Jaket',
-                'harga_beli' => 300000,
-                'harga_jual' => 700000,
+                'barang_id' => 2, // Sesuaikan dengan ID kategori Fashion
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'kategori_id' => 3, // Sesuaikan dengan ID kategori Barang
-                'barang_kode' => 'RT',
-                'barang_nama' => 'sapu',
-                'harga_beli' => 10000,
-                'harga_jual' => 15000,
+                'barang_id' => 3, // Sesuaikan dengan ID kategori Barang
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
