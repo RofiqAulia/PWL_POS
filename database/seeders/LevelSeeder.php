5287e1b600d6c7e38d3a9784daf99e1638a2b189
= <=>
2024-03-04 17:45:39 +0700

    first commit

--- database/seeders/LevelSeeder.php
+++ database/seeders/LevelSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
+
 
 class LevelSeeder extends Seeder
 {
@@ -23,32 +23,5 @@ class LevelSeeder extends Seeder
 
         DB::table('m_level')->insert($data);
     }
-    // public function run(): void
-    // {
-    //     $data = [
-    //         [
-    //             'user_id' => 1,
-    //             'level_id' => 1,
-    //             'username' => 'admin',
-    //             'nama' => 'Administrator',
-    //             'password' => Hash::make('12345'), // class untuk mengenkripsi/hash password
-    //         ],
-    //         [
-    //             'user_id' => 2,
-    //             'level_id' => 2,
-    //             'username' => 'manager',
-    //             'nama' => 'Manager',
-    //             'password' => Hash::make('12345'),
-    //         ],
-    //         [
-    //             'user_id' => 3,
-    //             'level_id' => 3,
-    //             'username' => 'staff',
-    //             'nama' => 'Staff/Kasir',
-    //             'password' => Hash::make('12345'),
-    //         ]
-    //     ];
-    
-    //     DB::table('m_user')->insert($data);
-    // }
+
 }    
