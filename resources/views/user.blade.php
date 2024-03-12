2d3c20c89780b8fe9bc45cc62f92e13540046998
= <=>
2024-03-12 22:43:38 +0700

    Diubah pada ...

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -23,7 +23,7 @@
                 <td>{{ $d->username }}</td>
                 <td>{{ $d->nama }}</td>
                 <td>{{ $d->level_id }}</td>
-                <td>{{ $d->level->level_id}}</td>
+                <td>{{ $d->level->level_kode}}</td>
                 <td>{{ $d->level->level_nama}}</td>
                 <td><a href="{{route('/user/ubah', $d->user_id)}}">Ubah</a>|<a href="{{route('/user/hapus', $d->user_id)}}">Hapus</a></td>
             </tr>
