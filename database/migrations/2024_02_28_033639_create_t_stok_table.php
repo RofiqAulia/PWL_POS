a81436f7cf4ecde739e93da80acf3b488b72f87d
= <=>
2024-04-17 09:54:41 +0700

    diubah pada ...

--- database/migrations/2024_02_28_033639_create_t_stok_table.php
+++ database/migrations/2024_02_28_033639_create_t_stok_table.php
@@ -12,15 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('t_stok', function (Blueprint $table) {
-            $table->bigIncrements('stok_id');
-            $table->unsignedBigInteger('barang_id');
-            $table->unsignedBigInteger('user_id');
+            $table->id('stok_id');
+            $table->unsignedBigInteger('barang_id')->index();
+            $table->unsignedBigInteger('user_id')->index();
             $table->dateTime('stok_tanggal');
             $table->integer('stok_jumlah');
             $table->timestamps();
+
+            $table->foreign('barang_id')->references('barang_id')->on('m_barang');
+            $table->foreign('user_id')->references('user_id')->on('m_user');
         });
     }
-    
+
     /**
      * Reverse the migrations.
      */
