c5bde4c1c4c4d82355325d62609a8fc41637901b
= <=>
2024-04-21 23:24:06 +0700

    Jobsheet 9

---
+++ resources/views/manager.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('subtitile', 'Manager')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', 'Manager')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header">Tampilan <?= (Auth::user()->level_id == 1)?'Admin':'Manager'; ?>
+                <div class="card-body">
+                    <h1>Login Sebagai:
+                        <?= (Auth::user()->level_id == 1)?'Admin':'Manager'; ?>
+                    </h1>
+                    <a href="{{ route('logout') }}">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('js')
+@endpush
