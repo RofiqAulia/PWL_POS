ce6aa07ea93cde6a3ed999f91e65ac4ee15784ca
= <=>
2024-04-21 15:06:43 +0700

    Jobsheet 9

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Contracts\Auth\Authenticatable;
+
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function index() {
+        // kita ambil data user lalu simpan pada variabel $user
+        $user = Auth::user();
+
+        // kondisi jika usernya ada
+        if ($user) {
+            // jika usernya memiliki level admin
+            if ($user->level_id == '1') {
+                return redirect()->intended('admin');
+            }
+            // jika usernya memiliki level manager
+            else if ($user->level_id == '2') {
+                return redirect()->intended('manager');
+            }
+        }
+        return view('login');
+    }
+
+    public function proses_login(Request $request) {
+        // kita buat validasi pada saat tombol login diklik
+        // validasinya username & password wajib diisi
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        // ambil data request username & password saja
+        $credential = $request->only('username', 'password');
+        //cek jika data username dan password valid (sesuai) dengan data
+        if (Auth::attempt($credential)) {
+            
+            // kalau berhasil simpan data usernya di variabel $user
+            $user = Auth::user();
+
+            // cek lagi jika level user admin maka arahkan ke halaman admin
+            if ($user->level_id == '1') {
+                return redirect()->intended('admin');
+            } 
+            // tapi jika level usernya user biasa maka arahkan kehalaman user
+            else if ($user->level_id == '2') {
+                return redirect()->intended('manager');
+            }
+            // jika belum ada role maka ke halaman /
+            return redirect()->intended('/');
+        }
+
+        // jika tidak ada data user yang valid maka kembalikan lagi ke halaman login
+        // pastikan kirim pesar error juga kalau login gagal yaa...
+        return redirect('login')
+        ->withInput()
+        ->withErrors(['login_gagal' => 'Pastikan kembali username dan password yang dimasukkan sudah benar']);
+    }
+
+    public function register() {
+        // tampilkan view register
+        return view('register');
+    }
+
+    // aksi form register
+    public function proses_register(Request $request) {
+        // kita buat validasi untuk proses register
+        // validasinya yaitu semua field wajid diisi
+        // validasi username harus unique atau tidak boleh ada duplicate username
+        $validator = Validator::make($request->all(), [
+            'nama' => 'required', 
+            'username' => 'required|unique:m_user', 
+            'password' => 'required', 
+        ]);
+
+        // kalau gagal kembali ke halaman registrasi dengan menampilkan pesan error
+        if ($validator->fails()) {
+            return redirect('/register')
+            ->withErrors($validator)
+            ->withInput();
+        }
+
+        // jika berhasil isi level & hash passwornya
+        $request['level_id'] = '2';
+        $request['password'] = Hash::make($request->password);
+
+        // masukkan semua data pada request ke table user
+        UserModel::create($request->all());
+
+        // kalau berhasil arahkan ke halaman login
+        return redirect()->route('login');
+    }
+
+    public function logout(Request $request) {
+        // logout itu harus menghapus sessionnya
+        $request->session()->flush();
+
+        // jalankan juga dungsi logout pada auth
+        Auth::logout();
+
+        // kembali ke halaman login
+        return Redirect('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,14 @@
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
-use App\Http\Controllers\MUserController;
 use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\POSController;
 use App\Http\Controllers\StokController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ManagerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -111,4 +113,25 @@ Route::group(['prefix' => 'penjualan'], function () {
     Route::get('/{id}/edit', [PenjualanController::class, 'edit']);
     Route::put('/{id}', [PenjualanController::class, 'update']);
     Route::delete('/{id}', [PenjualanController::class, 'destroy']);
+});
+
+// This route is Auth Controller
+Route::get('/login', [AuthController::class, 'index'])->name('login');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/proses_login', [AuthController::class, 'proses_login'])->name('proses_login');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('/proses_register', [AuthController::class, 'proses_register'])->name('proses_register');
+
+// Kita atur juga untuk middleware menggunakan group pada routing
+// didalamnya terdapat group untuk mengecek kondisi login
+// jika user yang login merupakan admin maka akan diarahkan ke AdminController
+// jika user yang login merupakan manager maka akan diarahkan ke UserController
+Route::group(['middleware' => ['auth']], function () {
+
+    Route::group(['middleware' => ['cek_login:1']], function () {
+        Route::resource('admin', AdminController::class);
+    });
+    Route::group(['middleware' => ['cek_login:2']], function () {
+        Route::resource('manager', ManagerController::class);
+    });
 });
